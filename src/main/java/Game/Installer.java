7c95d98c16956aa8d1de5046fe2984e2c9e38430
SKishchenko <userg@yandex.ru>
2024-08-05 00:31:05 +0300

    Add Hamcrest methods

--- src/main/java/Game/Installer.java
+++ src/main/java/Game/Installer.java
@@ -1,2 +1,76 @@
-package Game;public class Installer {
+package Game;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Installer {
+
+    static String installPath = "D:\\Games";
+    static StringBuilder log = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        createDir(installPath);
+        createDir(installPath + "\\src");
+        createDir(installPath + "\\res");
+        createDir(installPath + "\\savegames");
+        createDir(installPath + "\\temp");
+
+        createDir(installPath + "\\src\\main");
+        createDir(installPath + "\\src\\test");
+
+        createFile(installPath + "\\src\\main\\Main.java");
+        createFile(installPath + "\\src\\main\\Utils.java");
+
+        createDir(installPath + "\\res\\drawables");
+        createDir(installPath + "\\res\\vectors");
+        createDir(installPath + "\\res\\icons");
+
+        createFile(installPath + "\\temp\\tmp.txt");
+
+        logToFile(log,installPath + "\\temp\\tmp.txt");
+    }
+
+    public static void createDir(String dirPath) {
+        File dir = new File(dirPath);
+        if (dir.exists()) {
+            log("Папка: " + dirPath + " уже существует!");
+        } else if (dir.mkdir()) {
+            log("Создана папка: " + dirPath);
+        } else {
+            log("Не удалось создать папку: " + dirPath);
+        }
+    }
+
+    public static void createFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            if (file.createNewFile()) {
+                log("Создан файл: " + filePath);
+            } else {
+                log("Не удалось создать файл: " + filePath);
+            }
+        } catch (IOException e) {
+            log("Ошибка при создании файла: " + filePath);
+            e.printStackTrace();
+        }
+    }
+
+    public static void logToFile(StringBuilder msg, String fileName) {
+        try (FileWriter logWriter = new FileWriter(fileName, true)) {
+            logWriter.write(msg.toString()+"\n");
+        } catch (IOException e) {
+            System.err.println("Ошибка при записи лога: " + e.getMessage());
+        }
+    }
+
+    public static void log(String msg) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("[" + dtf.format(now) + "] <" + msg + ">");
+        log.append("[").append(dtf.format(now)).append("] <").append(msg).append(">").append("\n");
+    }
 }
--- src/test/java/Game/InstallerTests.java
+++ src/test/java/Game/InstallerTests.java
@@ -1,2 +1,109 @@
-package Game;public class InstallerTests {
+package Game;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class InstallerTests {
+
+    private static final String TEST_INSTALL_PATH = "D:\\GamesTest";
+    private static final String TEST_FILE_PATH = TEST_INSTALL_PATH + "\\temp\\tmp.txt";
+
+    @Before
+    public void setUp() {
+        // Удалить все созданные папки и файлы перед каждым тестом
+        deleteDirectory(new File(TEST_INSTALL_PATH));
+    }
+
+    @After
+    public void tearDown() {
+        // Удалить все созданные папки и файлы после каждого теста
+        deleteDirectory(new File(TEST_INSTALL_PATH));
+    }
+
+    @Test
+    public void testCreateDir() {
+        Installer.createDir(TEST_INSTALL_PATH);
+        Assert.assertTrue(new File(TEST_INSTALL_PATH).exists());
+    }
+
+    @Test
+    public void testCreateFile() {
+        Installer.createDir(TEST_INSTALL_PATH);
+        Installer.createDir(TEST_INSTALL_PATH + "\\temp");
+        Installer.createFile(TEST_FILE_PATH);
+        Assert.assertTrue(new File(TEST_FILE_PATH).exists());
+    }
+    @Test
+    public void testCreateFileWithHamcrest() {
+        Installer.createDir(TEST_INSTALL_PATH);
+        Installer.createDir(TEST_INSTALL_PATH + "\\temp");
+        Installer.createFile(TEST_FILE_PATH);
+        assertThat(new File(TEST_FILE_PATH).exists(),  is(true));
+    }
+
+    @Test
+    public void testLogToFile() throws IOException {
+        Installer.createDir(TEST_INSTALL_PATH);
+        Installer.createDir(TEST_INSTALL_PATH + "\\temp");
+        Installer.createFile(TEST_FILE_PATH);
+        String testMessage = "Тестовое сообщение";
+        Installer.log(testMessage);
+        Installer.logToFile(Installer.log, TEST_FILE_PATH);
+
+        File file = new File(TEST_FILE_PATH);
+        if (file.exists() && file.length() > 0) {
+           try (BufferedReader reader = new BufferedReader(new FileReader(TEST_FILE_PATH))) {
+                String line;
+                boolean found = false;
+                while ((line = reader.readLine()) != null) {
+                    if (line.contains(testMessage)) {
+                        found = true;
+                        break;
+                    }
+                }
+                Assert.assertTrue(found);
+            }
+        }else {
+            Assert.fail("Файл не существует или пустой");
+        }
+    }
+
+    @Test
+    public void testLog() {
+        String testMessage = "Тестовое сообщение";
+        Installer.log(testMessage);
+        Assert.assertTrue(Installer.log.toString().contains(testMessage));
+    }
+    @Test
+    public void testLogWithHamcrest() {
+        String testMessage = "Тестовое сообщение";
+        Installer.log(testMessage);
+        assertThat(Installer.log.toString(), containsString(testMessage));
+    }
+
+
+    private void deleteDirectory(File dir) {
+        if (dir.exists()) {
+            for (File file : dir.listFiles()) {
+                if (file.isDirectory()) {
+                    deleteDirectory(file);
+                } else {
+                    file.delete();
+                }
+            }
+            dir.delete();
+        }
+    }
 }
